6aa2a0927fabb056bee2f4479344b536af5fbe89
CharlesSchimmel <charlesschimmel@gmail.com>
2018-09-30 17:04:30 -0500

    added time type and bchan event. time elapsed now updates

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,7 @@ module Lib
 import           KodiRPC.Methods.Input as I
 import           KodiRPC.Calls
 import           KodiRPC.Types
+import           KodiRPC.Util
 
 import           Types                 as T
 
@@ -27,7 +28,7 @@ import           Data.Scientific
 import           Data.Text             as T hiding (intercalate)
 import           Debug.Trace           as Tr
 import           Graphics.Vty          as V
-import           Lens.Micro.Platform   ((&), (^.), (.~), set)
+import           Lens.Micro.Platform   ((&), (^.), (.~), set, over)
 import           Network.Socket        (withSocketsDo)
 import           Prelude               as P
 import qualified Brick.Widgets.Center  as C
@@ -44,14 +45,14 @@ app = App { appDraw = drawUI
 
 topLine ui = hBox [ padRight (Pad 1) . str $ timeString ui
                   , C.hCenter . withAttr attrTitle . str $ ui^.T.title
-                  , padLeft (Pad 1) . str $ "Vol: " ++ show (ui^.(player.volume)) ++ "%"
+                  , padLeft (Pad 2) . str $ "Vol: " ++ show (ui^.player.volume) ++ "%"
                   ]
 
-midLine ui = hBox [ C.hCenter . str $ isPlayingStr ui ]
+midLine ui = hBox [ padLeft (Pad 1) . str $ isPlayingStr ui ]
                   -- album if available, maybe 
 
 drawUI ui = [ topLine ui
-            , C.hCenterWith (Just '-') $ str "-"
+            -- , C.hCenterWith (Just '-') $ str "-"
             , midLine ui
             ]
 
@@ -68,32 +69,43 @@ theMap = attrMap V.defAttr [ (someText, fg cyan)
 --              -> handleKey
 --              -> handleChar
 
-handleEvent :: UI -> BrickEvent Name (Maybe Notif) -> EventM Name (Next UI)
-handleEvent ui (AppEvent x) = handleNotifs ui x
-handleEvent ui (VtyEvent x) = handleVtyEvent ui x
+handleEvent :: UI -> BrickEvent Name BChanEvent -> EventM Name (Next UI)
+handleEvent ui (AppEvent (P.Left x)) = continue $ handleTick ui
+handleEvent ui (AppEvent (P.Right x)) = handleNotif ui $ tdb x
+handleEvent ui (VtyEvent x) = handleVtyEvent ui $ tdb x
 handleEvent ui _            = continue ui
 
-handleNotifs :: UI -> Maybe Notif -> EventM Name (Next UI)
-handleNotifs ui (Just notif) = trace ( show notif ) $ notifMethodHandler p m ui
+handleTick :: KState -> KState
+handleTick ui = if notPlaying then ui else over (player.timeElapsed) addSec ui
+  where notPlaying = ui^.player.speed == 0.0
+
+handleNotif :: UI -> Maybe Notif -> EventM Name (Next UI)
+handleNotif ui (Just notif) = notifMethodHandler m p ui
   where p = notif^.notifParams
         m = notif^.notifMethod :: String
-handleNotifs ui _        = continue ui
+handleNotif ui _        = continue ui
 
-(#?) map key = HM.lookup key map
-(>#?) map key = map >>= lookup' key
-lookup' key (Object map) = HM.lookup key map
-lookup' _ _ = Nothing
+-- possible candidate for monad?
+updater fns p ks = P.foldr (\f -> f p) ks fns
+updateSpeed    p ks = ks & (player.speed) .~ extractSpeed p
+updatePlayerId p ks = ks & (player.playerId) .~ extractPlayerId p
+updateTitle    p ks = ks & title .~ T.unpack (extractTitle p)
+updateTime     p ks = ks & (player.timeElapsed) .~ extractTime p
+updateVolume   p ks = ks & (player.volume) .~ extractVolume p
 
-updateSpeed    params ks = ks & (player.speed) .~ extractSpeed params
-updatePlayerId params ks = ks & (player.playerId) .~ extractPlayerId params
-updateTitle    params ks = ks & title .~ T.unpack (extractTitle params)
+notifMethodHandler :: String -> Object -> UI -> EventM Name (Next UI)
+notifMethodHandler "Player.OnPause" p k = continue $ updater [updatePlayerId, updateSpeed] p k
 
-notifMethodHandler :: Object -> String -> UI -> EventM Name (Next UI)
-notifMethodHandler params "Player.OnPause"  ks = continue $ updatePlayerId params . updatePlayerId params $ ks
-notifMethodHandler params "Player.OnStop"   ks = continue $ updatePlayerId params . updatePlayerId params $ ks'
-  where ks' = ks & nowPlaying .~ Nothing
-notifMethodHandler params "Player.OnPlay"   ks = continue $ updatePlayerId params . updatePlayerId params $ ks'
-  where ks' = updateTitle params ks
+notifMethodHandler "Player.OnStop" p k = continue ks''
+  where ks' = k & nowPlaying .~ Nothing
+        ks'' = ks' & player .~ clearPlayer (ks'^.player)
+
+notifMethodHandler "Player.OnPlay" p k = continue $ updater [updatePlayerId, updateSpeed, updateTitle] p k
+  -- also update nowplaying to song/video
+  --
+notifMethodHandler "Player.OnSeek" p k = continue $ updater [updatePlayerId, updateSpeed, updateTime] p k
+notifMethodHandler "Application.OnVolumeChanged" p k = continue $ updater [updateVolume] p k
+
 --   -- may have to use suspendandresume here for updating state from IO
 --   -- where ks' = ks & nowPlaying .~ /not
 -- -- also call to get new player information
@@ -101,19 +113,20 @@ notifMethodHandler params "Player.OnPlay"   ks = continue $ updatePlayerId param
 notifMethodHandler _ _ ks                 = continue ks
 
 -- extractPlayerId :: Notif -> Float
-extractPlayerId p = fromMaybe 0.0 $ maybeSci <=< maybeValue $ p #? "data" >#? "player" >#? "playerid"
+extractPlayerId p = maybe 0.0 toRealFloat $ maybeNum $ p #? "data" >#? "player" >#? "playerid"
+extractSpeed p    = maybe 0.0 toRealFloat $ maybeNum $ p #? "data" >#? "player" >#? "speed"
+extractTitle p    = fromMaybe "n/a" $ maybeStr $ p #? "data" >#? "item" >#? "title"
+extractVolume p   = Volume pct mute
+  where pct       = truncate $ fromMaybe 0 $ maybeSci <=< maybeNum $ p #? "data" >#? "volume"
+        mute      = fromMaybe False $ maybeBool $ p #? "data" >#? "muted"
 
-extractSpeed p = fromMaybe 0.0 $ maybeSci <=< maybeValue $ p #? "data" >#? "player" >#? "speed"
-
-extractTitle p = fromMaybe "n/a" $ maybeStr $ p #? "data" >#? "item" >#? "title"
-
-maybeStr (Just(String s)) = Just s
-maybeStr _ = Nothing
-maybeValue (Just (Number n)) = Just n
-maybeValue _ = Nothing
-
-maybeSci :: Scientific -> Maybe Float
-maybeSci s = either Just (const Nothing) $ floatingOrInteger s
+extractTime p  = Time hrs mins sec mil
+  where time   = p #? "data" >#? "player" >#? "time"
+        hrs    = safe $ time >#? "hours"
+        mins   = safe $ time >#? "minutes"
+        sec    = safe $ time >#? "seconds"
+        mil    = safe $ time >#? "milliseconds"
+        safe t = fromMaybe 0 $ maybeInt <=< maybeNum $ t
 
 handleVtyEvent :: s -> Event -> EventM n (Next s)
 handleVtyEvent ui (V.EvKey (V.KChar c) []) = handleChar ui c
@@ -127,10 +140,10 @@ handleKey ui _        = continue ui
 
 handleChar :: s -> Char -> EventM n (Next s)
 -- movement
-handleChar ui 'h'  = trace "h" $ sAct ui I.Left
-handleChar ui 'j'  = trace "j" $ sAct ui I.Down
-handleChar ui 'k'  = trace "k" $ sAct ui I.Up
-handleChar ui 'l'  = trace "l" $ sAct ui I.Right
+handleChar ui 'h'  = sAct ui I.Left
+handleChar ui 'j'  = sAct ui I.Down
+handleChar ui 'k'  = sAct ui I.Up
+handleChar ui 'l'  = sAct ui I.Right
 -- window ctl
 handleChar ui '\t' = kallState ui $ xAct I.Fullscreen
 -- media ctl
@@ -153,17 +166,26 @@ kallState state action = suspendAndResume $ do
 
 -- populate all the basic kstate data
 initKState = KState "" p "Play something!" Nothing emptyObject
-  where p  = Player 0.0 0.0 [12,34] [45,67] 0
+  where p  = Player 0.0 0.0 te tr (Volume 100 False)
+        te = Time 0 12 34 0
+        tr = Time 0 45 67 0
 
 timeString kstate = elapsed ++ "/" ++ remaining
-  where elapsed   = intercalate ":" $ P.map P.show (plyer ^. timeElapsed)
-        remaining = intercalate ":" $ P.map P.show (plyer ^. timeRemaining)
+  where elapsed   = P.show (plyer ^. timeElapsed)
+        remaining = P.show (plyer ^. timeRemaining)
         plyer     = kstate ^. player
 
 xAct = I.executeAction
 
+-- will need another bchan for second ticks
 someFunc :: IO ()
+-- someFunc = justListenToNotifs
 someFunc = do
   chan <- newBChan 10
-  forkIO $ forever $ notification test >>= writeBChan chan
+  forkIO $ forever $ do
+    writeBChan chan $ P.Left Tick
+    threadDelay 1000000
+  forkIO $ forever $ P.Right <$> notification test >>= writeBChan chan
   void $ customMain (V.mkVty V.defaultConfig) (Just chan) app initKState
+
+justListenToNotifs = forever $ print =<< notification test
--- src/Types.hs
+++ src/Types.hs
@@ -5,6 +5,7 @@ module Types where
 
 import           Lens.Micro.Platform
 import           Data.Aeson
+import           KodiRPC.Types
 
 type Name = ()
 
@@ -38,12 +39,57 @@ data Player = Player
   , _playerId      :: Float
   , _timeElapsed   :: Time
   , _timeRemaining :: Time
-  , _volume        :: Int
+  , _volume        :: Volume
   }
   deriving (Show)
 
+data Volume = Volume
+  { _volPercent :: Int
+    , _muted :: Bool
+  }
 
-type Time = [Int]
+instance Show Volume where
+  show (Volume p m) = if m then "--" else if p < 100 then " " ++ padInt p else padInt p
+
+clearPlayer (Player _ _ _ _ e) = Player 0 0 mempty mempty e
+
+data Time = Time
+  { _hours         :: Hours
+  , _minutes       :: Minutes
+  , _seconds       :: Seconds
+  , _milliseconds  :: Milliseconds
+  }
+  deriving (Eq, Ord)
+
+instance Monoid Time where
+  mempty = Time 0 0 0 0
+  mappend _ b = b
+
+padInt x = if x < 10 then "0" ++ show x else show x
+
+instance Show Time where
+  show (Time 0 a b _) = padInt a ++ ":" ++ padInt b
+  show (Time a b c _) = padInt a ++ ":" ++ padInt b ++ ":" ++ padInt c
+
+type Hours        = Int
+type Minutes      = Int
+type Seconds      = Int
+type Milliseconds = Int
+
+data Tick = Tick
+  deriving ( Show )
+
+type BChanEvent = Either Tick (Maybe Notif)
+
+addSec :: Time -> Time
+addSec (Time h n s m) = Time h' n'' s'' m
+  where s' = s + 1
+        n' = if s' > 59 then n + 1 else n
+        h' = if n' > 59 then h + 1 else h
+        s'' = if s' > 59 then 0 else s'
+        n'' = if n' > 59 then 0 else n'
+
+
 
 makeLenses ''NowPlaying
 makeLenses ''Player
